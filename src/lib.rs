01dc80123d1a6410201be953b191c9ede2e43db1
Timo Kösters <timo@koesters.xyz>
2020-10-19 21:53:02 +0200

    Improve code style
    Co-authored-by: Lazalatin <lazalatin@tutanota.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,35 @@
 //! # JWT Decoder
 //!
 //! `jwt` is a JSON Web Token decoder, which makes debugging with JWTs more convenient.
-#![forbid(unsafe_code)]
 
-use base64;
-use std;
+use std::error::Error;
 
-pub fn run(args: &Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
-    let jwt_string = parse_arguments(args).unwrap();
-
-    if jwt_string == "-h" || jwt_string == "--help" {
-        printout_help();
-        std::process::exit(0);
-    } else {
-        let decoded_jwt: Vec<String> = process_jwt(jwt_string).expect("Something went wrong parsing the jwt");
-        printout_decoded_jwt(&decoded_jwt);
-        std::process::exit(0);
+pub fn run(args: &[String]) -> Result<(), Box<dyn Error>> {
+    match parse_arguments(args)? {
+        Some(argument) if argument != "-h" && argument != "--help" => {
+            let decoded_jwt = process_jwt(argument)?;
+            printout_decoded_jwt(&decoded_jwt);
+        }
+        _ => printout_help(),
     }
+
+    Ok(())
 }
 
 /// Processes arguments given to `jwt`
 ///
 /// # Examples
 /// ```
-/// let args: &Vec<String> = &vec!("/bin/jwt".to_string(), "--help".to_string());
+/// let args = ["/bin/jwt".to_string(), "--help".to_string()];
 ///
-/// assert_eq!(Ok("--help"), jwt_decode::parse_arguments(&args))
+/// assert_eq!(Ok(Some("--help")), jwt_decode::parse_arguments(&args))
 /// ```
-pub fn parse_arguments(args: &Vec<String>) -> Result<&str, &'static str> {
+pub fn parse_arguments(args: &[String]) -> Result<Option<&str>, &'static str> {
     if args.len() == 0 {
         return Err("Program was called illegally!");
-    } else if args.len() == 1 {
-        return Ok("--help");
-    } else {
-        let result: &str = &args[1];
-        return Ok(result);
     }
+
+    Ok(args.get(1).map(|s| &**s))
 }
 
 /// Processes a given JWT string by splitting it and decoding header and body
@@ -45,35 +39,35 @@ pub fn parse_arguments(args: &Vec<String>) -> Result<&str, &'static str> {
 /// ```
 /// let jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 ///
-/// assert_eq!(vec!["{\"alg\":\"HS256\",\"typ\":\"JWT\"}", "{\"sub\":\"1234567890\",\"name\":\"John Doe\",\"iat\":1516239022}"], jwt_decode::process_jwt(&jwt.to_string()).unwrap());
+/// assert_eq!(
+///     vec![r#"{"alg":"HS256","typ":"JWT"}"#, r#"{"sub":"1234567890","name":"John Doe","iat":1516239022}"#],
+///     jwt_decode::process_jwt(&jwt.to_string()).unwrap()
+/// );
 /// ```
-///
-/// # Panic
-/// This function may panic if the given string is not processable
-///
-pub fn process_jwt(jwt: &str) -> Result<Vec<String>, &'static str> {
-    let splitted_jwt_strings: Vec<&str> = jwt.split('.').collect();
+pub fn process_jwt(jwt: &str) -> Result<[String; 2], Box<dyn Error>> {
+    let splitted_jwt_strings: Vec<_> = jwt.split('.').collect();
 
-    let jwt_header = splitted_jwt_strings[0];
-    let jwt_body = splitted_jwt_strings[1];
+    let jwt_header = splitted_jwt_strings.get(0).expect("split always returns at least one element");
+    let jwt_body = splitted_jwt_strings.get(1).ok_or(Box::<dyn Error>::from("Could not find separator in jwt string."))?;
 
-    let decoded_jwt_header = base64::decode(jwt_header).unwrap();
-    let decoded_jwt_body = base64::decode(jwt_body).unwrap();
+    let decoded_jwt_header = base64::decode(jwt_header)?;
+    let decoded_jwt_body = base64::decode(jwt_body)?;
 
-    let converted_jwt_header = String::from_utf8(decoded_jwt_header).unwrap();
-    let converted_jwt_body = String::from_utf8(decoded_jwt_body).unwrap();
+    let converted_jwt_header = String::from_utf8(decoded_jwt_header)?;
+    let converted_jwt_body = String::from_utf8(decoded_jwt_body)?;
 
-    return Ok([converted_jwt_header, converted_jwt_body].to_vec());
+    Ok([converted_jwt_header, converted_jwt_body])
 }
 
-fn printout_decoded_jwt(jwt: &Vec<String>) {
+fn printout_decoded_jwt(jwt: &[String]) {
     println!("{}", jwt[0]);
     println!("{}", jwt[1]);
 }
 
 fn printout_help() {
     println!("{} {}\n", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
-    println!("jwt displays a given base64 encoded JWT String in a readable manner.\nJWT header and body are printed in separate lines.\n");
+    println!("jwt displays a given base64 encoded JWT String in a readable manner.\n
+        JWT header and body are printed in separate lines.\n");
     println!("Syntax: {} [Options] <JWT>\n", env!("CARGO_PKG_NAME"));
     println!("Options:\n\t--help,  -h\t View this help message");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 #![forbid(unsafe_code)]
-use std;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let args: Vec<_> = std::env::args().collect();
     if let Err(err) = jwt_decode::run(&args) {
-        eprintln!("An Error occurred: {}", err.to_string());
-        std::process::exit(1)
+        eprintln!("An error occurred: {:?}", err);
+        std::process::exit(1);
     }
 }
